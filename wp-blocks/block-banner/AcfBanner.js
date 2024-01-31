38c4898983769190fd3d09bd7aae1ad3758ec594
Quentin Vannarath <qvannarath@gmail.com>
2024-01-31 16:32:48 +0100

    💄 [BANNER] Change styles

--- wp-blocks/block-banner/AcfBanner.js
+++ wp-blocks/block-banner/AcfBanner.js
@@ -23,12 +23,30 @@ AcfBanner.fragments = {
 };
 
 const Section = styled.section`
+  display: flex;
+  align-items: center;
+  background-color: black;
   position: relative;
   overflow: hidden;
+  height: 100vh;
 
   .image {
+    position: absolute;
+    inset: 0;
+  }
+
+  .content {
     position: relative;
-    height: 30rem;
+    color: white;
+
+    h1 {
+      font-size: 4rem;
+      font-weight: 700;
+
+      @media (min-width: 1024px) {
+        font-size: 6rem;
+      }
+    }
   }
 `
 
@@ -48,16 +66,14 @@ export default function AcfBanner(props) {
   return (
     <Section>
       <div className='grid grid-cols-12 lg:grid-cols-24 gap-x-2.5 items-center'>
-        <div className='col-start-2 col-end-12 lg:col-start-3 lg:col-end-11 h-full lg:relative'>
-          <div className='image' ref={image}>
-            {props.blockBanner.image &&
-              <Image
-                src={props.blockBanner.image?.node.sourceUrl}
-                alt={props.blockBanner.image?.node.altText}
-                objectFit="cover"
-                layout='fill'
-              />}
-          </div>
+        <div className='image' ref={image}>
+          {props.blockBanner.image &&
+            <Image
+              src={props.blockBanner.image?.node.sourceUrl}
+              alt={props.blockBanner.image?.node.altText}
+              objectFit="cover"
+              layout='fill'
+            />}
         </div>
         <div className='content col-start-2 col-end-12 lg:col-start-12 lg:col-end-23 flex flex-col gap-y-5'>
           <h1>
